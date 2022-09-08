6eeb1190a1bcbda3f6cc769408b4a2d0c884b735
Herr Fristi <brzeskiproductions@gmail.com>
2022-09-08 16:35:45 +0200

    - initial barebones VR

---
+++ Plugin.cs
@@ -0,0 +1,26 @@
+﻿using BepInEx;
+using Valve.VR;
+
+namespace VRMaker
+{
+    [BepInPlugin(PluginInfo.PLUGIN_GUID, PluginInfo.PLUGIN_NAME, PluginInfo.PLUGIN_VERSION)]
+    public class Plugin : BaseUnityPlugin
+    {
+        public const string PLUGIN_GUID = "com.HerrFristi.VRMods.VRMaker";
+        public const string PLUGIN_NAME = "VRMaker";
+        public const string PLUGIN_VERSION = "0.0.1";
+
+        private void Awake()
+        {
+            // Plugin startup logic
+            Logger.LogInfo($"Plugin {PluginInfo.PLUGIN_GUID} is loaded!");
+            InitSteamVR();
+        }
+
+        private static void InitSteamVR()
+        {
+            SteamVR.Initialize();
+            SteamVR_Settings.instance.pauseGameWhenDashboardVisible = true;
+        }
+    }
+}
