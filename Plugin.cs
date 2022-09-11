ceaaa673c90efa29a78c2ea244ba66f18d8bf4db
Herr Fristi <brzeskiproductions@gmail.com>
2022-09-11 15:07:29 +0200

    - improved Firstperson Hook

--- Plugin.cs
+++ Plugin.cs
@@ -30,50 +30,6 @@ namespace VRMaker
             SteamVR_Settings.instance.pauseGameWhenDashboardVisible = true;
         }
 
-        // For temporary debug stuff
-        public void LateUpdate()
-        {
-
-            if (Input.GetKeyDown(KeyCode.F1))
-            {
-                Logger.LogInfo("F1 pressed");
-
-                if (!F1Pressed)
-                {
-                    F1Pressed = true;
-
-                    Camera MyCamera = Game.GetCamera();
-                    // If we are not in firstperson
-                    if (CameraManager.CurrentCameraMode != CameraManager.VRCameraMode.FirstPerson)
-                    {
-                        // switch to first person
-                        VROrigin.transform.position = Game.Instance.Player.MainCharacter.Value.GetPosition();
-                        OriginalCameraParent = MyCamera.transform.parent;
-                        MyCamera.transform.parent = VROrigin.transform;
-                        CameraManager.CurrentCameraMode = CameraManager.VRCameraMode.FirstPerson;
-                    }
-                    else
-                    {
-                        MyCamera.transform.parent = OriginalCameraParent;
-                        CameraManager.CurrentCameraMode = CameraManager.VRCameraMode.DemeoLike;
-                    }
-                }
-            }
-            else
-            {
-                F1Pressed = false;
-            }
-
-            if (CameraManager.CurrentCameraMode == CameraManager.VRCameraMode.FirstPerson)
-            {
-                VROrigin.transform.position = Game.Instance.Player.MainCharacter.Value.GetPosition();
-            }
-        }
-
-        bool F1Pressed = false;
-        Transform OriginalCameraParent = null;
-        GameObject VROrigin = new GameObject();
-
     }
 
 }
--- VRCamera/Patches/CameraPatches.cs
+++ VRCamera/Patches/CameraPatches.cs
@@ -19,5 +19,58 @@ namespace VRMaker
         {
             CameraManager.ReduceNearClipping();
         }
+
+        [HarmonyPostfix]
+        [HarmonyPatch(typeof(Kingmaker.UI.KeyboardAccess), "Tick")]
+        private static void CheckForPerspectiveToggle()
+        {
+            if (Input.GetKeyDown(KeyCode.F1))
+            {
+                Logs.WriteInfo("F1 pressed");
+
+                if (!F1Pressed)
+                {
+                    F1Pressed = true;
+
+                    Camera MyCamera = Game.GetCamera();
+                    // If we are not in firstperson
+                    if (CameraManager.CurrentCameraMode != CameraManager.VRCameraMode.FirstPerson)
+                    {
+                        if (Game.Instance.Player.MainCharacter != null)
+                        {
+                            // switch to first person
+                            VROrigin.transform.position = Game.Instance.Player.MainCharacter.Value.GetPosition();
+                            OriginalCameraParent = MyCamera.transform.parent;
+                            MyCamera.transform.parent = VROrigin.transform;
+                            CameraManager.CurrentCameraMode = CameraManager.VRCameraMode.FirstPerson;
+                        }
+                        
+                    }
+                    else
+                    {
+                        MyCamera.transform.parent = OriginalCameraParent;
+                        CameraManager.CurrentCameraMode = CameraManager.VRCameraMode.DemeoLike;
+                    }
+                }
+            }
+            else
+            {
+                F1Pressed = false;
+            }
+
+            if (CameraManager.CurrentCameraMode == CameraManager.VRCameraMode.FirstPerson)
+            {
+                if (Game.Instance.Player.MainCharacter != null)
+                {
+                    VROrigin.transform.position = Game.Instance.Player.MainCharacter.Value.GetPosition();
+                }
+                    
+            }
+        }
+
+        static bool F1Pressed = false;
+        static Transform OriginalCameraParent = null;
+        static GameObject VROrigin = new GameObject();
     }
+
 }
